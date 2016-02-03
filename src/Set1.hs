45bce4f9ff49c826c11c4001bebf8592b0a8f242
Kirill Kazakov <kirikaza@ecwid.com>
2016-02-03 22:05:58 +0300

    Solved exercise 1.2 "Random Numbers: Random Character Generation"

--- src/Set1.hs
+++ src/Set1.hs
@@ -5,6 +5,7 @@ module Set1 where
 
 import MCPrelude
 
+
 fiveRands :: [Integer]
 
 fiveRands = [a, b, c, d, e]
@@ -14,4 +15,18 @@ fiveRands = [a, b, c, d, e]
     (c,sc) = rand sb
     (d,sd) = rand sc
     (e,se) = rand sd
-    
+
+
+randLetter :: Seed -> (Char, Seed)
+randString3 :: String
+
+randLetter seed = (letter, nextSeed)
+  where
+    (num,nextSeed) = rand seed
+    letter = toLetter num
+
+randString3 = [a, b, c]
+  where
+    (a,sa) = randLetter $ mkSeed 1
+    (b,sb) = randLetter sa
+    (c,sc) = randLetter sb
