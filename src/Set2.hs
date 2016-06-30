520b3922eff0880e97ec545ecd764c1081a3eef6
Kirill Kazakov <kirikaza@ecwid.com>
2016-06-30 23:12:58 +0400

    Solved exercise 2.2 "Failing Computations: Build a library of things that can fail"

--- src/Set2.hs
+++ src/Set2.hs
@@ -12,3 +12,31 @@ instance Show a => Show (Maybe a) where
   show Nothing = "Nothing"
   show (Just x) = "Just " ++ show x
 
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:_) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (_:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay x (pair@(k, v):pairs) = if k == x then Just v else lookupMay x pairs
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0 = Nothing
+divMay p q = Just (p / q)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay (x:xs) = Just $ case maximumMay xs of
+  Nothing -> x
+  Just max -> if x > max then x else max
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay (x:xs) = Just $ case minimumMay xs of
+  Nothing -> x
+  Just min -> if x < min then x else min
